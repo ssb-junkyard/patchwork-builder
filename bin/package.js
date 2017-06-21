25399cda6706c5d070ecaa6512803621ab1ae4b2
Michael Williams <michael.williams@enspiral.com>
2017-06-22 01:37:00 +1200

    add script to build npm package
    - add electron dep from dev dep

---
+++ bin/package.js
@@ -0,0 +1,17 @@
+const { join } = require('path')
+const { readFileSync, writeFileSync } = require('fs')
+
+const path = process.argv[2] || process.cwd()
+
+addElectronDepFromDevDep(path)
+
+function addElectronDepFromDevDep (pkgPath) {
+  const pkgPath = join(path, 'package.json')
+  const pkgFile = readFileSync(pkgPath)
+  const pkg = JSON.parse(pkgFile)
+
+  pkg.dependencies.electron = pkg.devDependencies.electron
+
+  const nextPkgFile = JSON.stringify(pkg, null, 2)
+  writeFileSync(pkgPath, nextPkgFile)
+}
